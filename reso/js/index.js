24ba35f8e7051a54d7b2902411a84cff51492981
Macbook Pro <macbookpro@Macbooks-MacBook-Pro.local>
2025-09-16 21:59:19 +0700

    Update

--- reso/js/index.js
+++ reso/js/index.js
@@ -3,9 +3,18 @@ document.querySelectorAll('.note-card').forEach(card => {
   const content = card.querySelector('.note-card-content');
 
   header.addEventListener('click', () => {
-    card.classList.toggle('expanded');
+    if (card.classList.contains('expanded')) {
+      // collapse
+      content.style.maxHeight = 0;
+      card.classList.remove('expanded');
+    } else {
+      // expand
+      content.style.maxHeight = content.scrollHeight + "px";
+      card.classList.add('expanded');
+    }
   });
 });
+
 
 document.querySelectorAll('.note-image').forEach(img => {
   img.addEventListener('click', () => {
